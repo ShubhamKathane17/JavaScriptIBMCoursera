1c736519dc4060b64f5083ce25b74f291cc31064
ShubhamKathane17 <shubhamkathaneabd17@gmail.com>
2024-09-06 09:13:26 -0400

    created grocery tracker

---
+++ grocery/grocery_Tracker.js
@@ -0,0 +1,12 @@
+let g1;
+let g2;
+let g3;
+
+function groceryTracker(){
+    g1 = parseFloat(document.getElementById('grocery1').value);
+    g2 = parseFloat(document.getElementById('grocery2').value);
+    g3 = parseFloat(document.getElementById('grocery3').value);
+
+    let totalAmount = g1 + g2 + g3;
+    document.getElementById('result').innerText = `The total amount is:$ ${area}`;
+}
