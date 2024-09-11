869bea41aa8dc800b133cd645184a2739281e8bb
ShubhamKathane17 <shubhamkathaneabd17@gmail.com>
2024-09-11 03:14:06 -0400

    added deletebutton

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,9 +25,14 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick="deleteBook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
+}
+function deleteBook(index) {
+    books.splice(index, 1);
+    showbooks();
 }
 
 function clearInputs() {
@@ -36,3 +41,5 @@ function clearInputs() {
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
 }
+
+
