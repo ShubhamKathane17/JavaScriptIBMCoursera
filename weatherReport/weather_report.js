6c6bde62e3e42effd8602352e7c948f8a978c878
ShubhamKathane17 <shubhamkathaneabd17@gmail.com>
2024-09-14 03:35:10 -0400

    Added latitude and longitude to the code

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,12 +1,24 @@
-
-
 function showweatherDetails(event) {
     event.preventDefault();
 
     const city = document.getElementById('city').value;
-    const apiKey = '61538d8c3bd81d2cf860681066aa5702'; // Replace 'YOUR_API_KEY' with your actual API key
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    
+    const lat = document.getElementById('lattitude').value;
+    const lon = document.getElementById('longitude').value;
+
+    const apiKey = '61538d8c3bd81d2cf860681066aa5702'; // Replace with your actual API key
+    let apiUrl;
+
+    // Check if latitude and longitude are provided, otherwise use city
+    if (lat && lon) {
+        apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    } else if (city) {
+        apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    } else {
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Please enter either a city or both latitude and longitude.</p>`;
+        return;
+    }
+
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
